e4d1fe6df8369592b6f5a4cee28865a07e39ac9e
savalle <savalle@GCFRIDF017>
2026-02-02 13:02:46 +0100

    modif player et ennemie

--- Territorial War/Player.cpp
+++ Territorial War/Player.cpp
@@ -1,1 +1,19 @@
 #include "Player.h"
+
+Player::Player() : Entity(sf::CircleShape(20.f))
+{
+    shape.setFillColor(sf::Color::Red);
+    shape.setPosition({ 400.f, 300.f });
+}
+
+void Player::update(float speed)
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Up))
+        move({ 0.f, -speed });
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Down))
+        move({ 0.f, speed });
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Left))
+        move({ -speed, 0.f });
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::Right))
+        move({ speed, 0.f });
+}
