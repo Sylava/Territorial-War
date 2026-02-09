fefb5f9667032c806f54107d5901fa7997cb8bff
ajacquemet-levina <ajacquemet-levina@GCFRIDF016>
2026-02-09 10:13:13 +0100

    correction de Patrol.cpp

--- Territorial War/PatrolState.cpp
+++ Territorial War/PatrolState.cpp
@@ -1,41 +1,66 @@
-#include "PatrolState.h"
 #include "NPC.h"
-#include "Map.h"
-#include <random>
+#include "Conditions.h"
 
-void PatrolState::Enter(NpcContext& context)
+void Npc::Init(Map* map, Player* player)
 {
-    std::cout << "Enter Patrol State" << std::endl;
-    context.reachedPoint = false;
-    context.npc->isMoving = true;
-    context.reachedPoint = false;
-    setPatrolPoints(context);
+    context.npc = this;
+    context.map = map;
+    context.player = player;
+    PatrolState* patrolState = fsm.CreateState<PatrolState>();
+    ChaseState* chaseState = fsm.CreateState<ChaseState>();
+    IdleState* idleState = fsm.CreateState<IdleState>();
+
+    idleState->AddTransition(Conditions::IsSeeingPlayer, chaseState);
+    idleState->AddTransition(Conditions::HasWaited, patrolState);
+    patrolState->AddTransition(Conditions::IsSeeingPlayer, chaseState);
+    patrolState->AddTransition(Conditions::HasReachedPoint, idleState);
+    chaseState->AddTransition([](NpcContext& _context)
+        {
+            return !Conditions::IsSeeingPlayer(_context);
+        }, idleState);
+
+    fsm.Init(patrolState, context);
 }
 
-void PatrolState::Execute(NpcContext& context, const float dt)
+void Npc::move(const sf::Vector2f& move, const Map* map)
 {
-    sf::Vector2f dir = (pointToGo - context.npc->position).normalized();
-    context.npc->move(dir * context.npc->speed * dt, context.map);
-
-    if (getDistance(pointToGo, context.npc->position) < 5.f || !context.npc->isMoving)
-        context.reachedPoint = true;
+    if (move.x > 0.f)
+        animMirror = true;
+    else if (move.x < 0.f)
+        animMirror = false;
+    sf::Vector2f oldPosition = position;
+    moveOnAxis(position.x, hitbox.position.x, move.x, map->left, map->right, map);
+    moveOnAxis(position.y, hitbox.position.y, move.y, map->top, map->bottom, map);
+    if (animMirror)
+        attackArea.setPosition({ position.x + 25, position.y });
+    else
+        attackArea.setPosition({ position.x - 25, position.y });
+    if (position == oldPosition)
+        isMoving = false;
 }
 
-void PatrolState::Exit(NpcContext& context)
+void Npc::moveOnAxis(float& pos, float& hitboxPos, float move, float min, float max, const Map* map)
 {
+    float oldPosition = pos;
+    float oldHitbox = hitboxPos;
 
-}
-
-void PatrolState::setPatrolPoints(NpcContext& context)
-{
-    float y = context.map->top + rand() % ((int)context.map->bottom - (int)context.map->top + 1);
-    float x = context.map->left + rand() % ((int)context.map->right - (int)context.map->left + 1);
-    pointToGo = sf::Vector2f(x, y);
-}
-
-float PatrolState::getDistance(const sf::Vector2f& a, const sf::Vector2f& b)
-{
-    float dx = b.x - a.x;
-    float dy = b.y - a.y;
-    return std::sqrt(dx * dx + dy * dy);
+    pos += move;
+    hitboxPos += move;
+    if (hitboxPos > min && hitboxPos < max)
+    {
+        for (const sf::Sprite& rock : map->rocks)
+        {
+            if (hitbox.findIntersection(rock.getGlobalBounds()))
+            {
+                pos = oldPosition;
+                hitboxPos = oldHitbox;
+                break;
+            }
+        }
+    }
+    else
+    {
+        pos = oldPosition;
+        hitboxPos = oldHitbox;
+    }
 }
