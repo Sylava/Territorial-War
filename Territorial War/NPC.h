2bbe36bbd2796d9e02c99c586566b2a9e0e0667f
ajacquemet-levina <ajacquemet-levina@GCFRIDF016>
2026-02-09 10:12:31 +0100

    correction NPC.h

--- Territorial War/NPC.h
+++ Territorial War/NPC.h
@@ -1,75 +1,9 @@
-//#pragma once
-//#include <SFML/Graphics.hpp>
-//#include <random>
-//#include "Entity.h"
-//#include "Statemachine.h"
-//#include "NpcContext.h"
-//
-//class Npc : public Entity<sf::RectangleShape>
-//{
-//public:
-//    Npc();
-//    ~Npc();
-//
-//    void Init();
-//    void update(float dt) override;
-//
-//private:
-//    sf::Vector2f getDirection(const sf::Vector2f& v);
-//    void chooseRandomTarget();
-//
-//private:
-//    FSM::StateMachine<NpcContext> fsm;
-//    NpcContext context;
-//
-//    sf::Vector2f target;     // point aléatoire à atteindre
-//    sf::Vector2f direction;  // direction normalisée
-//    float speed = 100.f;
-//
-//    // Générateur aléatoire
-//    std::mt19937 gen;
-//    std::uniform_int_distribution<int> distX;
-//    std::uniform_int_distribution<int> distY;
-//};
-
-
-//#pragma once
-//#include <SFML/Graphics.hpp>
-//#include "StateMachine.h"
-//#include "ChaseState.h"
-//#include "Conditions.h"
-//#include "NpcContext.h"
-//#include "PatrolState.h"
-//#include "Entity.h"
-//
-//using namespace NpcAi;
-//
-//class Npc : public Entity<sf::RectangleShape>
-//{
-//public:
-//    Npc();
-//    ~Npc() = default;
-//
-//    void Init();
-//    void update(float dt, const sf::RenderWindow& window);
-//
-//    // Utilitaire
-//    static sf::Vector2f Normalize(const sf::Vector2f& v);
-//
-//    // Données
-//    FSM::StateMachine<NpcContext> fsm;
-//    sf::Vector2f direction;
-//    float speed = 100.f;
-//};
-
 #pragma once
-
 #include "StateMachine.h"
 #include "ChaseState.h"
 #include "NpcContext.h"
-#include "IdleState.h"
 #include "PatrolState.h"
-
+#include "IdleState.h"
 #include "Map.h"
 #include "Player.h"
 
@@ -78,7 +12,6 @@ using namespace NpcAi;
 class Npc
 {
 public:
-
 
     FSM::StateMachine<NpcContext> fsm;
     NpcContext context{};
